595b0843512b5e56350ce0266788f7a5381357a8
Peter <wrkpeter@gmail.com>
2019-01-10 11:51:00 +0100

    init(rotttenEgss): init

---
+++ src/main/java/rotteneggs/fedexday/FedexDayApplication.java
@@ -0,0 +1,14 @@
+package rotteneggs.fedexday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedexDayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FedexDayApplication.class, args);
+  }
+
+}
+
---
+++ src/test/java/rotteneggs/fedexday/FedexDayApplicationTests.java
@@ -0,0 +1,17 @@
+package rotteneggs.fedexday;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FedexDayApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
